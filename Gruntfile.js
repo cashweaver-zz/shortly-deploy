36b09f0a079e088299db29caaaa13f0347ac7c81
Hack Reactor Students <accounts@hackreactor.com>
2016-10-13 15:47:53 -0700

    Fix mistyped path

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
     uglify: {
       my_target: {
         files: {
-          'public/dest/app.min.js': ['public/dest/app.js'],
+          'public/dist/app.min.js': ['public/dist/app.js'],
         }
       }
     },
